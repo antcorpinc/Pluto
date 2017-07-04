ae4efcd6dc2ad8ba80481705a7a5b774c7de8c24
Roni <antcorpinc@gmail.com>
2017-07-04 11:40:40 +0530

    Creating initial FrameworkBody component

--- Pluto.Web/ClientApp/src/app/app.module.ts
+++ Pluto.Web/ClientApp/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {FwModule} from './fw/fw.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FwModule
   ],
   providers: [],
   bootstrap: [AppComponent]
