c535f5e2edadc133926bf3fe7047e3288bb362b4
Roni <antcorpinc@gmail.com>
2017-07-04 14:34:58 +0530

    Added framework titlebar component

--- Pluto.Web/ClientApp/src/app/fw/fw.module.ts
+++ Pluto.Web/ClientApp/src/app/fw/fw.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
 
 @NgModule({
   imports: [
@@ -9,7 +10,8 @@ import { ContentComponent } from './content/content.component';
   ],
   declarations: [
     FrameworkBodyComponent,
-    ContentComponent],
+    ContentComponent,
+    TitleBarComponent],
   exports:[
     FrameworkBodyComponent
   ]
