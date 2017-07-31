92443963adadcc6d8a4a0df1e3ddbbc9f2ff54b5
Roni <antcorpinc@gmail.com>
2017-07-31 13:59:03 +0530

    Implementing SignOut Feature

--- Pluto.Web/ClientApp/src/app/fw/top-bar/top-bar.component.ts
+++ Pluto.Web/ClientApp/src/app/fw/top-bar/top-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FrameworkConfigService} from '../services/framework-config.service';
+import { UserApi} from '../users/user-api';
 
 @Component({
   selector: 'fw-top-bar',
@@ -8,10 +9,15 @@ import {FrameworkConfigService} from '../services/framework-config.service';
 })
 export class TopBarComponent implements OnInit {
 
-  //Info: Need to inject as public since its properties are used in template
-  constructor(public frameworkConfigService:FrameworkConfigService) { }
+  //Info: Need to inject as public since its properties are used in template for AOT
+  constructor(public frameworkConfigService:FrameworkConfigService,
+  public userApi:UserApi) { }
 
   ngOnInit() {
+  }
+
+  signOut(){
+    this.userApi.signOut();
   }
 
 }
--- Pluto.Web/ClientApp/src/app/fw/users/user-api.ts
+++ Pluto.Web/ClientApp/src/app/fw/users/user-api.ts
@@ -4,6 +4,8 @@ export abstract class UserApi {
 
  abstract signIn(username:string,password:string,rememberMe:boolean): Observable<any>;
 
+ abstract signOut():Observable<any>;
+
   //signout
   //change password
 }
