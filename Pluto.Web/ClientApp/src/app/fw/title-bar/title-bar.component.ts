cb21980084074d9dfc5eaf39615f43edce784ef2
Roni <antcorpinc@gmail.com>
2017-08-11 11:58:47 +0530

    Adding panel component in framework & new sample logo

--- Pluto.Web/ClientApp/src/app/fw/title-bar/title-bar.component.ts
+++ Pluto.Web/ClientApp/src/app/fw/title-bar/title-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ScreenService} from '../services/screen.service';
 import {MenuService} from '../services/menu.service';
+import { UserApi} from '../users/user-api';
 
 @Component({
   selector: 'fw-title-bar',
@@ -12,7 +13,8 @@ export class TitleBarComponent implements OnInit {
   //Info: Add public members in () when their properties needed in
   // template -- Needed for AOT
   constructor(public screenService:ScreenService,
-  public menuService:MenuService) { }
+  public menuService:MenuService,
+  public userApi:UserApi) { }
 
   ngOnInit() {
   }
