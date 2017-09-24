419224d8f899875c9c821be4114d61f835a08101
antcorpinc <antcorpinc@gmail.com>
2017-09-24 12:49:50 +0530

    Adding initial Api Controller

--- Pluto.Web/ClientApp/src/app/fw/content/content.component.ts
+++ Pluto.Web/ClientApp/src/app/fw/content/content.component.ts
@@ -3,14 +3,15 @@ import {MenuService} from '../services/menu.service';
 import {ScreenService} from '../services/screen.service';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'fw-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
 
-  constructor(public menuService:MenuService,
-              public screenService:ScreenService) { }
+  constructor(public menuService: MenuService,
+              public screenService: ScreenService) { }
 
   ngOnInit() {
   }
