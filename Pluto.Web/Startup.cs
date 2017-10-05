ecbb6e2e0a6a5ad638d7f0bd307ccd85b582cdaa
Roni <antcorpinc@gmail.com>
2017-10-05 11:38:18 +0530

    Added initial Seed data for identity

--- Pluto.Web/Startup.cs
+++ Pluto.Web/Startup.cs
@@ -1,25 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Pluto.Data;
+using Pluto.Models;
 
 namespace Pluto.Web
 {
     public class Startup
     {
+
+        private readonly IHostingEnvironment _environment;
+        public IConfigurationRoot _configurationRoot { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            _environment = env;
+
+            builder.AddEnvironmentVariables();
+            _configurationRoot = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<PlutoContext>(
+                options =>
+                {
+                    options.UseSqlServer(
+                        _configurationRoot.GetConnectionString("PlutoContextConnection"));
+                });
+
+            services.AddTransient<PlutoContextSeedData>();
+            
+            services.AddIdentity<PlutoUser, PlutoRole>()
+                    .AddEntityFrameworkStores<PlutoContext>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            PlutoContextSeedData seeder )
         {
             if (env.IsDevelopment())
             {
@@ -38,6 +67,8 @@ namespace Pluto.Web
                 routes.MapSpaFallbackRoute("spa-fallback",
                 new {controller="Home", action="Index"});
             });
+
+            seeder.SeedAsync().Wait();
 
             /*app.Run(async (context) =>
             {
